70818c0af15ba76206f405feac88d82d02a0ca10
Andrew Astapov <andrey.astapov@graphics.cs.msu.ru>
2021-04-26 17:10:23 +0300

    Buttons: todo added

--- Sources/EButton.py
+++ Sources/EButton.py
@@ -6,17 +6,20 @@ from UserInput import UserInput
 class EButton(EUIElement):
     def __init__(self, name, position, size):
         super().__init__()
-        self.__position = position
         self.__textureName = name
+        # @TODO: replace position and size by pygame.Rect
+        self.__position = position
         self.__size = size
         self.__enable = False
-        self.drawOrder = 3
+        # @TODO: change drawOrder
+        self.drawOrder = 100
 
     def update(self):
         super().update()
         input = UserInput()
         if input.is_mouse_down():
             mousePosition = input.get_mouse_position()
+            # @TODO: check collision by pygame rectangle
             if 0 < mousePosition[0] - self.__position[0] < self.__size[0] and \
                0 < mousePosition[1] - self.__position[1] < self.__size[1]:
                 self.__enable = True
