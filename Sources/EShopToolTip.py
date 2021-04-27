fa3989f8501f46e23e0f0b920e4a1de015e5083e
Lev Smirnov <lyovasmirnov@gmail.com>
2021-04-27 21:09:24 +0300

    Add ShopToolTip

---
+++ Sources/EShopToolTip.py
@@ -0,0 +1,11 @@
+import pygame
+
+class EShopToolTip(EToolTip):
+    def __init__(self, position, size):
+        super().__init__(position, size)
+
+    def update(self):
+        super().update()
+
+    def draw(self):
+        super().draw()
---
+++ Sources/EToolTip.py
@@ -0,0 +1,13 @@
+import pygame
+
+from EUIElement import EUIElement
+
+class EToolTip(EUIElement):
+    def __init__(self, position, size):
+        super().__init__(position, size)
+
+    def update(self):
+        super().update()
+
+    def draw(self):
+        super().draw()
