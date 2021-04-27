f87dd3bfc82f30dce84adc845a5428757ebcedb4
Andrew Astapov <andrey.astapov@graphics.cs.msu.ru>
2021-04-27 04:34:38 +0300

    ESource: added effect of nearby trees
    System of neighbours' effects is WIP

--- Sources/ESource.py
+++ Sources/ESource.py
@@ -1,5 +1,7 @@
 from EBuilding import EBuilding
 from EntitySystem import EntitySystem
+from EGrid import EGrid
+from ETree import ETree
 from enum import Enum
 
 class ESourceType(Enum):
@@ -13,10 +15,11 @@ class ResourceModifyingInfo:
         self.tickPeriod   = tickPeriod #in update ticks
 
 class ESourceInfo:
-    def __init__(self, spriteName, effectRadius, modifyingResourses):
-        self.spriteName        = spriteName
-        self.effectRadius      = effectRadius
+    def __init__(self, spriteName, effectRadius, modifyingResourses, neighbourEffects):
+        self.spriteName         = spriteName
+        self.effectRadius       = effectRadius
         self.modifyingResources = modifyingResourses
+        self.neighbourEffects   = neighbourEffects
 
 class ESource(EBuilding):
     nameToTypeDict = {
@@ -24,10 +27,12 @@ class ESource(EBuilding):
         "Sawmill"  : ESourceType.SAW_MILL
     }
     typeInfoDict = {
-        ESourceType.WIND_MILL : ESourceInfo("WindMill", 5, []),
+        ESourceType.WIND_MILL : ESourceInfo("WindMill", 5, [], {}),
         ESourceType.SAW_MILL  : ESourceInfo("Sawmill", 2, [
             ResourceModifyingInfo("Wood", 1, 120)
-        ])
+        ],{
+            "Tree" : 1 #@TODO: refactor this
+        })
     }
 
     def __init__(self, x, y, sourceType):
@@ -38,12 +43,14 @@ class ESource(EBuilding):
         self.__effectRadius  = self.__typeInfo.effectRadius
         self.__modifyingResources = self.__typeInfo.modifyingResources
         self.__tickCounters = [ 0 for _ in self.__modifyingResources ]
+        self.__onTickResourceCounts = [modRes.onTickCount for modRes in self.__modifyingResources]
 
     def update(self):
         super().update()
         for i, modRes in enumerate(self.__modifyingResources):
             if self.__tickCounters[i] >= modRes.tickPeriod:
-                self.__resourcePanel.add_resource(modRes.resourceName, modRes.onTickCount)
+                onTickCount = self.__onTickResourceCounts[i]
+                self.__resourcePanel.add_resource(modRes.resourceName, onTickCount)
                 self.__tickCounters[i] = 0
 
         self.__tickCounters = [i+1 for i in self.__tickCounters]
@@ -58,4 +65,23 @@ class ESource(EBuilding):
 
     def set_pos(self, coord):
         super().set_pos(coord)
-        self.__tickCounter = 0
+        self.__tickCounters = [i+1 for i in self.__tickCounters]
+        self.__onTickResourceCounts = [modRes.onTickCount for modRes in self.__modifyingResources]
+
+        grid = EntitySystem().get_grid()
+        cellPos = grid.world_to_cell((self.x, self.y)) #@TODO: maybe it's ok to use self.__cell_x from EOnGrid
+
+        for y in range(cellPos[1] - self.__effectRadius, cellPos[1] + self.__effectRadius + 1):
+            for x in range(cellPos[0] - self.__effectRadius, cellPos[0] + self.__effectRadius + 1):
+                if x == cellPos[0] and y == cellPos[1]:
+                    continue
+                neighbourIds = grid.get_cell((x,y))
+                if neighbourIds is None:
+                    continue
+                for neighbourId in neighbourIds:
+                    if neighbourId > 0:
+                        neighbour = EntitySystem().get_entity(neighbourId)
+                        if isinstance(neighbour, ETree):
+                            if "Tree" in self.__typeInfo.neighbourEffects:
+                                treeEffect = self.__typeInfo.neighbourEffects["Tree"]
+                                self.__onTickResourceCounts = [tickCount + treeEffect for tickCount in self.__onTickResourceCounts]
