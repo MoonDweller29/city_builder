62b6d9f3b17d8793fbec4d83f28bb6fb290d23c9
AlexHonor <berserk323@yandex.ru>
2021-04-29 03:55:51 +0300

    Flake

--- city_builder/Sources/EUIElement.py
+++ city_builder/Sources/EUIElement.py
@@ -10,7 +10,7 @@ class EUIElement(Entity):
 
     def is_inside(self, coord):
         return 0 <= coord[0] - self._position[0] <= self._size[0] and \
-        0 <= coord[1] - self._position[1] <= self._size[1]
+            0 <= coord[1] - self._position[1] <= self._size[1]
 
     def update(self):
         super().update()
