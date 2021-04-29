9277128609e450985a910ed157f6df9f97020aa4
Andrew Astapov <38720432+MoonDweller29@users.noreply.github.com>
2021-04-28 22:41:08 -0300

    Smrinov/docstyle (#40)
    * Add dockstyle
    
    * Utils: added dots at the end of docstring sentences
    
    * Utils: fixing error D202 from pydocstyle
    
    deleted blink lines after docstrings
    
    * Utils: fixing error D100 from pydocstyle
    
    fixed missing docstring at the top of module
    
    * debug branch is removed from target action branches
    
    Co-authored-by: Lev Smirnov <lyovasmirnov@gmail.com>

--- city_builder/Sources/Utils.py
+++ city_builder/Sources/Utils.py
@@ -1,3 +1,5 @@
+"""Module contains functions of vector operations on tuples and other useful ones."""
+
 import math
 import operator
 from typing import Tuple
@@ -6,78 +8,71 @@ from typing import Tuple
 # Vector operations on tuples
 
 def add(a: Tuple, b: Tuple) -> Tuple:
-    """Calculate element-wise sum of tuples of equal size
+    """Calculate element-wise sum of tuples of equal size.
 
     :param a: tuple of size N which contains objects with overloaded operator.__add__
     :param b: tuple of size N which contains objects with overloaded operator.__add__
     :return: tuple of size N with element-wise sum of tuples a and b
     """
-
     return tuple(map(operator.add, a, b))
 
 
 def sub(a: Tuple, b: Tuple) -> Tuple:
-    """Calculate element-wise subtraction of tuple b from tuple a
+    """Calculate element-wise subtraction of tuple b from tuple a.
 
     :param a: tuple of size N which contains objects with overloaded operator.__sub__
     :param b: tuple of size N which contains objects with overloaded operator.__sub__
     :return: tuple of size N with element-wise subtraction of tuple b from tuple a
     """
-
     return tuple(map(operator.sub, a, b))
 
 
 def mul(a: Tuple, b: Tuple) -> Tuple:
-    """Calculate element-wise multiplication of tuples of equal size
+    """Calculate element-wise multiplication of tuples of equal size.
 
     :param a: tuple of size N which contains objects with overloaded operator.__mul__
     :param b: tuple of size N which contains objects with overloaded operator.__mul__
     :return: tuple of size N with element-wise multiplication of tuples a and b
     """
-
     return tuple(map(operator.mul, a, b))
 
 
 def div(a: Tuple, b: Tuple) -> Tuple:
-    """Calculate element-wise division of tuple a by tuple b
+    """Calculate element-wise division of tuple a by tuple b.
 
     :param a: tuple of size N which contains objects with overloaded operator.__div__
     :param b: tuple of size N which contains objects with overloaded operator.__div__
     :return: tuple of size N with element-wise division of tuple a by tuple b
     """
-
     return tuple(map(operator.truediv, a, b))
 
 
 def floor(x: Tuple) -> Tuple:
-    """Calculate element-wise math.floor from tuple x
+    """Calculate element-wise math.floor from tuple x.
 
     :param x: tuple of floats or objects with overloaded __floor__() method
     :return: tuple of floored elements of x
     """
-
     return tuple(map(math.floor, x))
 
 
 def to_int(a: Tuple[float, float]) -> Tuple[int, int]:
-    """Cast tuple of 2 floats to tuple of 2 ints
+    """Cast tuple of 2 floats to tuple of 2 ints.
 
     :param a: tuple of 2 float elements
     :return: tuple of 2 int elements
     """
-
     return (int(a[0]), int(a[1]))
 
 
 def lerp(t: float, a: float, b: float) -> float:
-    """Perform linear interpolation between floats a and b with coefficient t
+    """Perform linear interpolation between floats a and b with coefficient t.
 
     :param t: float in range [0,1], t==0 => a, t==1 => b
     :param a: first value for linear interpolation
     :param b: second value for linear interpolation
     :return: interpolated value between a and b
     """
-
     if (t > 1):
         return b
 
@@ -88,12 +83,11 @@ def lerp(t: float, a: float, b: float) -> float:
 
 
 def ease_out_elastic(t: float) -> float:
-    """Calculate Out Elastic easing from time t
+    """Calculate Out Elastic easing from time t.
 
     :param t: time in range [0, 1]
     :return: calculated easing value
     """
-
     c4 = (2 * math.pi) / 3
 
     if t < 0:
